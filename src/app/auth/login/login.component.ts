94b8371b4fb183b2e4f88558363e5109679f034e
Sandra Moya Ortega <san.mo.dra5@gmail.com>
2024-12-11 10:34:04 +0100

    login task edit

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,13 +11,6 @@ export class LoginComponent {
 
 // TODO: validate all the shield in the client side. 
 
-/*
-  - All fields are required. 
-  - emails fields must be of a type email. 
-  - password must have at least 4 characters. 
-  - emails are equals (message: repeat email).
-*/
-
 // TODO: geolocate. 
 
 }
