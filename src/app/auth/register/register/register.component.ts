d46d5ee697866af1053bfb8fdb6b0aea9333ab95
Sandra Moya Ortega <san.mo.dra5@gmail.com>
2024-12-11 10:34:19 +0100

    Register: create and task

---
+++ src/app/auth/register/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'register',
+  standalone: true,
+  imports: [],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+// TODO: validate all the shield in the client side. 
+
+/*
+  - All fields are required. 
+  - emails fields must be of a type email. 
+  - password must have at least 4 characters. 
+  - emails are equals (message: repeat email).
+*/
+
+// TODO: geolocate. 
+}
